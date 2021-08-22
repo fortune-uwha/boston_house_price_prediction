c3135bb233e9907871af449e93215146bf45784c
Fortune Uwha <fortune.uwha@gmail.com>
2021-08-22 07:12:19 +0100

    Update utility function

---
+++ utils/input_processor.py
@@ -0,0 +1,13 @@
+import numpy as np
+import json
+
+
+def process_input(request_data: str) -> np.array:
+    """
+    A processing function to validate that the request data is correct.
+    :param request_data: request made to the API
+    :return: numpy array
+    """
+    parsed_body = np.asarray(json.loads(request_data)["inputs"])
+    assert len(parsed_body) >= 1 #"'inputs' must be a 1-d array with 13 parameters"
+    return parsed_body
